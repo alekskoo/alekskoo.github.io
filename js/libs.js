c2ccb5fc0019cd689a9ac0fbe0086a24c5c5c976
alekskoo <a.c.kucherenko@gmail.com>
2019-04-26 07:33:26 +0300

    modal

--- js/libs.js
+++ js/libs.js
@@ -168,11 +168,29 @@
 		removalDelay: 300,
 		mainClass: 'my-mfp-zoom-in'
 	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
 });
 
 
 	//counter initialization
 	$(document).ready(function() {
-	$('.counter').counterUp();
+	$('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
 });
 		
